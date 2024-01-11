74b8f758c18f500e7bacc5c9afa20f0077cddc7d
vasanam <uma.vasana@gmail.com>
2024-01-11 10:20:05 -0600

    setup

---
+++ src/main/java/com/vasana/cicdaws/CicdAwsApplication.java
@@ -0,0 +1,13 @@
+package com.vasana.cicdaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasana/cicdaws/CicdAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasana.cicdaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
